f75f0310ca5c5a4531911b57576a512c5d4204e1
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-18 21:37:15 -0400

    insert data into database

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,21 +3,30 @@ const { Client } = require("pg");
 const SQL = `
 CREATE TABLE IF NOT EXISTS categories (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 ) NOT NULL
+  name VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS items (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   category_id INTEGER REFERENCES categories (id) NOT NULL,
   name VARCHAR ( 255 ) NOT NULL,
-  description VARCHAR (255) NOT NULL,
+  description TEXT NOT NULL,
   price NUMERIC NOT NULL CONSTRAINT positive_price CHECK(price > 0)
 );
+
+INSERT INTO categories (name)
+VALUES
+  ('Computers & Tablets'),
+  ('Cell Phones'),
+  ('Video Games'),
+  ('Cameras'),
+  ('Wearable Technology');
+
+
+INSERT INTO items (category_id, name, description, price)
+VALUES
+((SELECT id from categories WHERE name = 'Computers & Tablets'), 'Apple 14" MacBook Pro (M5 Pro, Space Black)', 'Now with the powerful M5 Pro chip, the Apple 14" MacBook Pro delivers advanced single- and multithreaded CPU performance and faster unified memory. Designed for scientists, engineers, software developers, and creative pros tackling intensive projects, the M5 Pro 15-Core chip features a next-generation 16-Core GPU with a Neural Accelerator in each core, which helps speed up AI tasks like LLM prompt processing and on-device transformer model training. The M5 Pro chip also brings up to 2x faster SSD performance than the previous generation for tasks that include importing RAW image files or exporting videos. Additionally, it offers optimal battery life of up to 24 hours, so you can take your pro workflows anywhere.', 3099.00);
 `;
-
-// const SQL = `DROP TABLE items;
-// DROP TABLE categories;`;
-
 async function main() {
   console.log("seeding...");
   const client = new Client({
