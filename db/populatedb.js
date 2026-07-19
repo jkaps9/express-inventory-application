6a05938a564b8ddfb2446550636f29cecc04f7dc
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-18 21:06:25 -0400

    setup structure

--- db/populatedb.js
+++ db/populatedb.js
@@ -3,19 +3,34 @@ const { Client } = require("pg");
 const SQL = `
 CREATE TABLE IF NOT EXISTS categories (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 )
+  name VARCHAR ( 255 ) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS items (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR ( 255 ),
-  category_id INTEGER REFERENCES categories(id),
-  price INTEGER
+  category_id INTEGER REFERENCES categories (id) NOT NULL,
+  name VARCHAR ( 255 ) NOT NULL,
+  description VARCHAR (255) NOT NULL,
+  price NUMERIC NOT NULL CONSTRAINT positive_price CHECK(price > 0)
 );
-
-INSERT INTO usernames (username) 
-VALUES
-  ('Bryan'),
-  ('Odin'),
-  ('Damon');
 `;
+
+// const SQL = `DROP TABLE items;
+// DROP TABLE categories;`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    host: "localhost", // or wherever the db is hosted
+    user: process.env.DATABASE_USER,
+    database: "my_store",
+    password: process.env.DATABASE_PASSWORD,
+    port: 5432, // The default port
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
