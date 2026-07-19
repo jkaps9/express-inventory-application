3b841acfd6386c77f98d080e70f90745ea440517
Josh <15219491+jkaps9@users.noreply.github.com>
2026-07-18 23:42:13 -0400

    create global style sheet

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+const assetsPath = path.join(__dirname, "public");
 
+app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
